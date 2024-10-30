0f189094ef02263229ac8630cdd7f27d40edac1b
Arkhipov Vyacheslav <arvykhip@gmail.com>
2024-10-30 11:22:38 +0300

    delete useless variable

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN: str = os.getenv("TOKEN")
-CHAT_ID: str = os.getenv("CHAT_ID")
 
 DB_USER_TABLE: str = os.getenv("DB_USER_TABLE")
 DB_DATA: str = os.getenv("DB_DATA")
