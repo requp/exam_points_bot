9512928c2e14cb4154bb4f602b13ae85c668b6bd
Arkhipov Vyacheslav <arvykhip@gmail.com>
2024-10-30 20:27:34 +0300

    Изменена логика всех запросов, добавлена запись баллов ко всем предметам ЕГЭ

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import telebot
 from telebot.types import Message
 
-from db_logic import save_user_data_query, get_user_query, save_exam_score_query
+from constant_variables import MIN_SCORES, MAX_SCORES, EXAM_NAMES
+from db_logic import (
+    save_user_data_query,
+    get_user_query,
+    save_exam_score_query,
+    get_exam_scores_query
+)
 from settings import TOKEN
 
-MAX_SCORES = 100
-MIN_SCORES = 0
 
 bot = telebot.TeleBot(token=TOKEN)
 
@@ -19,32 +23,31 @@ def send_welcome(message: Message):
         "указать свои баллы ЕГЭ и посмотреть их. "
         "Для регистрации введи команду /registr"
     )
-    print(message)
 
 
 @bot.message_handler(commands=['registr'])
 def registration(message: Message):
-    user_data: None | tuple = get_user_query(
+    res_flag, user_data = get_user_query(
         user_id=message.from_user.id,
-        requested_fields="first_name, last_name"
     )
-    if isinstance(user_data, tuple):
-        first_name, last_name = user_data
-        bot.reply_to(
-            message,
-            f"Ты уже зарестирован под именем {first_name} {last_name}."
-        )
-    else:
-        bot.reply_to(
-            message,
-            f"Для регистрации введи своё имя и фамилию. "
-            "Введи своё имя и фамилию как это указано ниже \nИван Иванов"
-        )
-        bot.register_next_step_handler(message, handle_registration)
+    if res_flag:
+        if user_data:
+            db_user_id, first_name, last_name = user_data[0]
+            bot.reply_to(
+                message,
+                f"Ты уже зарестирован под именем {first_name} {last_name}."
+            )
+        else:
+            bot.reply_to(
+                message,
+                f"Для регистрации введи своё имя и фамилию. "
+                "Введи своё имя и фамилию как это указано ниже \nИван Иванов"
+            )
+            bot.register_next_step_handler(message, handle_registration)
 
 
 def handle_registration(message: Message):
-    if ' ' not in message.any_text or len(message.any_text.split()) != 2:
+    if len(message.any_text.split()) != 2:
         bot.reply_to(
             message,
             "Твоё имя и фамилия не содержат пробел или не состоят из 2 слов. "
@@ -59,84 +62,134 @@ def handle_registration(message: Message):
                 "Попробуй ещё раз. Для регистрации введи команду /registr"
             )
         else:
-            bot.reply_to(
-                message,
-                f"Добро пожаловать {first_name} {last_name}!"
-                "Для того, чтобы ввести свои баллы введи команду /enter_scores"
-            )
-            save_user_data_query(
+            res_flag = save_user_data_query(
                 user_id=message.from_user.id, first_name=first_name, last_name=last_name
             )
+            if res_flag:
+                bot.reply_to(
+                    message,
+                    f"Добро пожаловать {first_name} {last_name}!"
+                    "Для того, чтобы ввести свои баллы введи команду /enter_scores"
+                )
 
 
 @bot.message_handler(commands=['enter_scores'])
 def enter_scores(message: Message):
-    user_data: None | tuple = get_user_query(
+    res_flag, user_data = get_user_query(
         user_id=message.from_user.id,
-        requested_fields="id, first_name, last_name, score"
     )
-    if user_data is None:
-        bot.reply_to(
-            message,
-            "Ты еще не зарегистрирован. Для регистрации введи /registr"
-        )
-    else:
-        db_user_id, first_name, last_name, score = user_data
-        if score:
+    if res_flag:
+        if not user_data:
             bot.reply_to(
                 message,
-                f"Ты уже ввёл баллы по экзамену. Кол-во баллов: {score}."
+                "Ты еще не зарегистрирован. Для регистрации введи /registr"
             )
         else:
+            exam_names: str = f"{', '.join(EXAM_NAMES[:-2])} {EXAM_NAMES[-1]}"
             bot.reply_to(
                 message,
-                f"Введи число в диапозоне от {MIN_SCORES} до {MAX_SCORES}."
+                "Ниже приведён список предметов ЕГЭ, которые можно внести:\n"
+                f"{exam_names}.\n"
+                "Напиши название экзамена и кол-во баллов как показано в примере:\n"
+                "Русский язык 90"
             )
             bot.register_next_step_handler(message, handle_enter_scores)
 
 
 def handle_enter_scores(message: Message):
-    if not message.any_text.isdigit() or not (MIN_SCORES < int(message.any_text) < MAX_SCORES):
+    exam_info: list = message.text.split()
+    if not (2 <= len(exam_info) <= 3):
         bot.reply_to(
             message,
-            f"Ты можешь ввести только целое число в диапозоне от {MIN_SCORES} до {MAX_SCORES}. "
+            f"Текст должен состоять из названия предмета и числа баллов. "
             f"Для ввода баллов введи команду /enter_scores"
         )
     else:
-        res: bool = save_exam_score_query(
-            user_id=message.from_user.id,
-            exam_score=int(message.any_text)
-        )
-        if res:
+        # Проверяем строку на наличие предмета из 2 слов (напр. русский язык)
+        if len(exam_info) == 3:
+            exam_name, exam_score = f"{exam_info[0]} {exam_info[1]}", exam_info[2]
+        else:
+            exam_name, exam_score = exam_info
+        if not exam_score.isdigit():
             bot.reply_to(
                 message,
-                f"Баллы сохранены успешно"
+                f"Текст должен состоять из названия предмета и числа баллов. "
+                f"Для ввода баллов введи команду /enter_scores"
             )
+        else:
+            exam_score = int(exam_score)
+            if not (MIN_SCORES <= exam_score <= MAX_SCORES):
+                bot.reply_to(
+                    message,
+                    f"Ты можешь ввести только целое число в диапозоне от {MIN_SCORES} до {MAX_SCORES}. "
+                    f"Для ввода баллов введи команду /enter_scores"
+                )
+            elif exam_name.lower() not in EXAM_NAMES:
+                bot.reply_to(
+                    message,
+                    f"Введенный тобой название не входит в список предметов. "
+                    f"Для ввода баллов введи команду /enter_scores"
+                )
+            else:
+                res_flag, user_data = get_user_query(
+                    user_id=message.from_user.id,
+                )
+                if res_flag:
+                    db_user_id, first_name, last_name = user_data[0]
+                    res_flag, exam_info = get_exam_scores_query(
+                        user_id=db_user_id,
+                    )
+                    if res_flag:
+                        for exam in exam_info:
+                            if exam[0] == exam_name:
+                                bot.reply_to(
+                                    message,
+                                    f"{first_name} {last_name}. Этот экзамен уже записан\n"
+                                    f"{exam[0].capitalize()} -- {exam[1]}"
+                                )
+                                break
+                        else:
+                            res_flag = save_exam_score_query(
+                                user_id=db_user_id,
+                                exam_name=exam_name,
+                                exam_score=exam_score
+                            )
+                            if res_flag:
+                                bot.reply_to(
+                                    message,
+                                    f"Баллы успешно сохранены"
+                            )
 
 
 @bot.message_handler(commands=['view_scores'])
 def view_scores(message: Message):
-    user_data: None | tuple = get_user_query(
+    flag_res, user_data = get_user_query(
         user_id=message.from_user.id,
-        requested_fields="id, first_name, last_name, score"
     )
-    if user_data is None:
-        bot.reply_to(
-            message,
-            "Ты еще не зарегистрирован. Для регистрации введи /registr"
-        )
-    else:
-        db_user_id, first_name, last_name, score = user_data
-        if not score:
+    if flag_res:
+        if not user_data:
             bot.reply_to(
                 message,
-                "Ты ещё не заполнил поле баллов. Для заполнения введи команду /enter_scores"
+                "Ты еще не зарегистрирован. Для регистрации введи /registr"
             )
         else:
-            bot.reply_to(
-                message,
-                f'{first_name} {last_name}, кол-во баллов: {score}'
-            )
+            db_user_id, first_name, last_name = user_data[0]
+            res_flag, user_exams = get_exam_scores_query(user_id=db_user_id)
+            exam_data: dict = dict((name, score) for name, score in user_exams)
+            if not exam_data:
+                bot.reply_to(
+                    message,
+                    "Ты ещё не заполнил поле баллов. Для заполнения введи команду /enter_scores"
+                )
+            else:
+                exam_table: str = ''
+                for name, score in exam_data.items():
+                    exam_table += f'{name.capitalize()} -- {score}\n'
+                bot.reply_to(
+                    message,
+                    f"{first_name} {last_name}, твоя таблица заполенных баллов ЕГЭ\n"
+                    f"{exam_table}"
+                )
 
 
 bot.polling(none_stop=True)
