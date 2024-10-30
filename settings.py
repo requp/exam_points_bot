55c3ed82b105f4935428b0959bc6062bc93fc7e6
Arkhipov Vyacheslav <arvykhip@gmail.com>
2024-10-30 20:23:37 +0300

    добавление константых переменных и ключа для названия второй таблицы

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 TOKEN: str = os.getenv("TOKEN")
 
+DB_EXAM_TABLE: str = os.getenv('DB_EXAM_TABLE')
 DB_USER_TABLE: str = os.getenv("DB_USER_TABLE")
 DB_DATA: str = os.getenv("DB_DATA")
